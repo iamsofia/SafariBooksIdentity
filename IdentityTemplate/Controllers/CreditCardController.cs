568f836f5ec7cca634bf9b6f6495431d0405616f
iamsofia <sofia.morenocesar@gmail.com>
2015-12-01 01:46:04 -0600

    User info in Controller

--- IdentityTemplate/Controllers/CreditCardController.cs
+++ IdentityTemplate/Controllers/CreditCardController.cs
@@ -9,6 +9,7 @@ using System.Web.Mvc;
 using IdentityTemplate.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Threading.Tasks;
 
 namespace IdentityTemplate.Controllers
 {
@@ -25,28 +26,38 @@ namespace IdentityTemplate.Controllers
         }
 
         // GET: /CreditCard/
+        //Get CreditCard(s) for the logged in user
         public ActionResult Index()
         {
-            return View(db.CreditCards.ToList());
-
+           
             var currentUser = manager.FindById(User.Identity.GetUserId());
             return View(db.CreditCards.ToList().Where(creditcard => creditcard.User.Id == currentUser.Id));
 
         }
-        //stopped here
 
+        // GET: /ToDo/All
+        [Authorize(Roles = "Manager")]
+        public async Task<ActionResult> All()
+        {
+            return View(await db.CreditCards.ToListAsync());
+        }
 
         // GET: /CreditCard/Details/5
-        public ActionResult Details(int? id)
+        public async Task<ActionResult> Details(int? id) 
         {
+            var currentUser = await manager.FindByIdAsync(User.Identity.GetUserId()); //mod
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            CreditCard creditcard = db.CreditCards.Find(id);
+            CreditCard creditcard = await db.CreditCards.FindAsync(id);
             if (creditcard == null)
             {
                 return HttpNotFound();
+            }
+            if (creditcard.User.Id != currentUser.Id)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.Unauthorized);
             }
             return View(creditcard);
         }
@@ -62,11 +73,14 @@ namespace IdentityTemplate.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include="CreditCardID,CardName,CardNumber,CardType")] CreditCard creditcard)
+        public async Task<ActionResult> Create([Bind(Include="User_Id, CreditCardID,CardName,CardNumber,CardType")] CreditCard creditcard)
         {
+            var currentUser = await manager.FindByIdAsync(User.Identity.GetUserId()); 
             if (ModelState.IsValid)
             {
+                creditcard.User = currentUser;
                 db.CreditCards.Add(creditcard);
+                await db.SaveChangesAsync();
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
@@ -75,16 +89,22 @@ namespace IdentityTemplate.Controllers
         }
 
         // GET: /CreditCard/Edit/5
-        public ActionResult Edit(int? id)
+        public async Task<ActionResult> Edit(int? id)
         {
+            var currentUser = await manager.FindByIdAsync(User.Identity.GetUserId()); 
             if (id == null)
             {
+                
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            CreditCard creditcard = db.CreditCards.Find(id);
+            CreditCard creditcard = await db.CreditCards.FindAsync(id);
             if (creditcard == null)
             {
                 return HttpNotFound();
+            }
+            if (creditcard.User.Id != currentUser.Id)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.Unauthorized);
             }
             return View(creditcard);
         }
@@ -94,28 +114,33 @@ namespace IdentityTemplate.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include="CreditCardID,CardName,CardNumber,CardType")] CreditCard creditcard)
+        public async Task<ActionResult> Edit([Bind(Include = "User_Id,CreditCardID,CardName,CardNumber,CardType")] CreditCard creditcard)
         {
             if (ModelState.IsValid)
             {
                 db.Entry(creditcard).State = EntityState.Modified;
-                db.SaveChanges();
+                await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
             return View(creditcard);
         }
 
         // GET: /CreditCard/Delete/5
-        public ActionResult Delete(int? id)
+        public async Task<ActionResult> Delete(int? id)
         {
+            var currentUser = await manager.FindByIdAsync(User.Identity.GetUserId()); 
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            CreditCard creditcard = db.CreditCards.Find(id);
+            CreditCard creditcard =  await db.CreditCards.FindAsync(id);
             if (creditcard == null)
             {
                 return HttpNotFound();
+            }
+            if (creditcard.User.Id != currentUser.Id)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.Unauthorized);
             }
             return View(creditcard);
         }
@@ -123,11 +148,11 @@ namespace IdentityTemplate.Controllers
         // POST: /CreditCard/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public async Task<ActionResult> DeleteConfirmed(int id)
         {
-            CreditCard creditcard = db.CreditCards.Find(id);
+            CreditCard creditcard = await db.CreditCards.FindAsync(id);
             db.CreditCards.Remove(creditcard);
-            db.SaveChanges();
+            await db.SaveChangesAsync();
             return RedirectToAction("Index");
         }
 
